13e7189d4bd6e46fbdd10ad50866c4a63dfdab84
ryanmaddockdev <github@mail.ryanmaddock.dev>
2024-12-13 09:51:08 +0000

    Added some destructure changes

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,20 +1,23 @@
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
 
 const App = () => {
-  const name = "Harry";
-  const age = 24;
+  const name = "Ryan";
+  const age = 29;
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Ryan" age={10 + 19} />
       <Hello name={name} age={age} />
     </div>
   );
